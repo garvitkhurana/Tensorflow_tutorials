be66b8c7666b5d9eda64a575e1df7b992121af73
Garvit Khurana <garvit.khurana@gmail.com>
2018-06-30 20:54:57 +0530

    no message

--- 01_tensorflow_basics.py
+++ 01_tensorflow_basics.py
@@ -2,22 +2,52 @@ import tensorflow as tf
 
 # creates nodes in a graph
 # "construction phase"
-x1 = tf.constant(5)
-x2 = tf.constant(6)
 
 
-result = x1*x2
-result=tf.multiply(x1,x2)
+
+x = tf.constant(5)
+y = tf.constant(6)
+
+
+'''----------------------------------------------------------------------'''
+## other way
+
+x=tf.Variable(5,name="x")
+y=tf.Variable(6,name="y")
+
+
+result = x*y
+## other way
+result=tf.multiply(x,y)
 
 print(result)
 
 # defines our session and launches graph
 sess = tf.Session()
 # runs result
+sess.run(x.initializer)
+sess.run(y.initializer)
 print(sess.run(result))
 sess.close()
 
+'''----------------------------------------------------------------------'''
+
 ## other way
 with tf.Session() as session:
+    tf.global_variables_initializer().run()
     ans=session.run(result)
     print(ans)
+
+'''----------------------------------------------------------------------'''
+
+w = tf.constant(3) 
+x=w+2 
+y=x+5 
+z=x*3
+with tf.Session() as sess: 
+    print(sess.run(y))
+    print(z.eval()) ## other way
+
+
+'''----------------------------------------------------------------------'''
+
